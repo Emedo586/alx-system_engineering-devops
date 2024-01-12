cfaf14a353b9327da736f3ee0439b03572cff0e5
Emedo586 <emedopeace99@gmail.com>
2024-01-12 02:41:54 -0800

    WEB SERVERS

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,24 @@
 #!/usr/bin/env bash
-# Configure server using puppet
+# Configuring and Automating server using puppet
 
-# defines a Puppet class called nginx_server that 
-#  encapsulates the configuration for the Nginx server.
-class nginx_server {
-  package { 'nginx':
-    ensure => installed,
-  }
+package { 'nginx':
+  ensure => installed,
+}
+
+# manages the Nginx configuration file located at /etc/nginx/sites-available/default
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/Emedo586 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
 
 #  manages the Nginx service.
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => Package['nginx'],
-  }
-# manages the Nginx configuration file located at /etc/nginx/sites-available/default.
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => "
-      server {
-        listen      80 default_server;
-        listen      [::]:80 default_server;
-        root        /var/www/html;
-        index       index.html index.htm;
-
-        location / {
-          return 200 'Hello World!';
-        }
-
-        location /redirect_me {
-          return 301 http://cuberule.com/;
-        }
-      }
-    ",
-    notify => Service['nginx'],
-  }
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
-#  includes the nginx_server class, ensuring that it gets applied.
-include nginx_server
-
